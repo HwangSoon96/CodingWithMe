e22aa7746082ffb9d9705b9c8676255a42ed0f4a
seung <38664481+seunggil1@users.noreply.github.com>
2022-07-26 22:53:04 +0900

    update compiler, request/result format

--- compiler/compile/c.js
+++ compiler/compile/c.js
@@ -3,12 +3,17 @@ const exec = util.promisify(require('child_process').exec);
 const fs = require('fs').promises;
 
 // do it(error check);
-exports.check = async (code,inFile,resultFile) => {
-  let answer;
+exports.check = async (code,inputData,answerData) => {
   let sourceFile;
+  let inputFile;
+  let answerFile;
   try {
-    answer = await fs.readFile(resultFile,'utf-8');
-    sourceFile = await fs.writeFile('a.c',code);
+    sourceFile = await fs.writeFile('main.c',code);
+    await fs.writeFile('input.in', inputData);
+    await fs.writeFile('output.out', answerData);
+
+    inputFile = await fs.readFile('input.in','utf-8');
+    answerFile = await fs.readFile('output.out','utf-8');
   } catch (error) {
     return {
       time : 0,
@@ -18,27 +23,27 @@ exports.check = async (code,inFile,resultFile) => {
   }
 
   try {
-    await exec("gcc a.c");
+    await exec("gcc main.c");
     let pre_time = Date.now();
-    let run = await exec(`./a.out < ${inFile}`);
+    let run = await exec(`./main.out < ${inputFile}`);
     let cur_time = Date.now();
 
     try {
-      await fs.unlink('a.c');
-      await fs.unlink('a.out');
+      await fs.unlink('main.c');
+      await fs.unlink('main.out');
     } catch (error) { }
 
     return {
       time : cur_time - pre_time,
       output : run.stdout,
-      success : (run.stdout == answer)
+      success : (run.stdout.trim() == answerFile)
     };
 
   } catch (error) {
     
     try {
-      await fs.unlink('a.c');
-      await fs.unlink('a.out');
+      await fs.unlink('main.c');
+      await fs.unlink('main.out');
     } catch (error) { }
 
     return {
